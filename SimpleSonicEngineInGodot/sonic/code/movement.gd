ade2bf52df7f1160d56a4779c618d0057c6d0a47
ops400 <therealops400@gmail.com>
2022-08-10 19:43:08 -0300

    Added gravity, jump and animations

--- SimpleSonicEngineInGodot/sonic/code/movement.gd
+++ SimpleSonicEngineInGodot/sonic/code/movement.gd
@@ -1,6 +1,10 @@
 extends KinematicBody2D
 
+onready var animationPlayer = $AnimationPlayer
+
 var speed = 255
+var gravity = 10
+var jump = 25*gravity
 #var moving = false
 var velocity = Vector2()
 #var right =  Vector2(1, 0)
@@ -11,11 +15,17 @@ var UP = Vector2(0, -1)
 var acceleration = 1
 
 func _physics_process(delta):
+	velocity.y += gravity
 	if(Input.is_action_pressed("right")):
 		velocity.x = min(velocity.x + acceleration, speed)
+		animationPlayer.play("run2R")
 	elif(Input.is_action_pressed("left")):
 		velocity.x = max(velocity.x - acceleration, -speed)
+		animationPlayer.play("run2L")
 	else:
 		velocity.x = lerp(velocity.x, 0, 0.09)
+	if(Input.is_action_just_pressed("jump")):
+		velocity.y -= jump
+		animationPlayer.play("ballR")
 	print(velocity.x)
-	velocity = move_and_slide(velocity, Vector2(0, 0))
+	velocity = move_and_slide(velocity, UP)
